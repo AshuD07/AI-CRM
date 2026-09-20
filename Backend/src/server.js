959f6a42a9a16ae2e9dd7e7019ea58e76a140d47
AshitoshD10 <ashudhaygude2@gmail.com>
2026-09-21 00:36:52 +0530

    Initial Commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,5 @@
-const app = require("./App");
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
